db4466ff20bd8f7d8007f58b271f8c1704cee2db
sscdotopen <ssc.open@googlemail.com>
2022-05-27 15:29:08 +0200

    prep for lin-opt

--- run-opt-benchmarks.sh
+++ run-opt-benchmarks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17326
-python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17322
+#python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17326
+#python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17322
 
-python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17326
-python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17322
+#python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17326
+#python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17322
 
 python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17326
 python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17322
