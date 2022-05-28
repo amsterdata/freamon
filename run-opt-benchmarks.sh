6c442f91bebd4901b529f5c2d236e62ead23a331
sscdotopen <ssc.open@googlemail.com>
2022-05-28 09:23:44 +0200

    towards more experiments

--- run-opt-benchmarks.sh
+++ run-opt-benchmarks.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17326
-#python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17322
+# Required on our Azure machine due to some dependency issues
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 
-#python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17326
-#python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17322
+python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17326
+python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17322
+
+python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17326
+python benchmarks--data-valuation.py pipelines--openml--multiplei.py 44 17322
 
 python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17326
 python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17322
