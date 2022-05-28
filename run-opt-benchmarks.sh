5675fb7b7c7006328a9f584d98146c51777078b4
sscdotopen <ssc.open@googlemail.com>
2022-05-28 09:27:02 +0200

    experiments

--- run-opt-benchmarks.sh
+++ run-opt-benchmarks.sh
@@ -7,7 +7,7 @@ PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17322
 
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17326
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--data-valuation.py pipelines--openml--multiplei.py 44 17322
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17322
 
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17326
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17322
