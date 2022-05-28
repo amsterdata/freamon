76ace766ff85beb2864defeacc00c7aa5148977d
sscdotopen <ssc.open@googlemail.com>
2022-05-28 09:24:52 +0200

    towards more experiments

--- run-opt-benchmarks.sh
+++ run-opt-benchmarks.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Required on our Azure machine due to some dependency issues
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 
-python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17326
-python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17322
 
-python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17326
-python benchmarks--data-valuation.py pipelines--openml--multiplei.py 44 17322
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17326
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17322
 
-python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17326
-python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17322
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17326
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--data-valuation.py pipelines--openml--multiplei.py 44 17322
+
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17326
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17322
