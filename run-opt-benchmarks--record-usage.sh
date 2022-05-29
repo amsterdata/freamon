9211ca9aa04652b33cd8eeedbd98915a3bac027f
sscdotopen <ssc.open@googlemail.com>
2022-05-29 11:00:02 +0200

    exp

--- run-opt-benchmarks--record-usage.sh
+++ run-opt-benchmarks--record-usage.sh
@@ -20,10 +20,5 @@
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--mlinspect--credit.py
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--mlinspect--product-images.py
 
-
-
-#PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17326
-#PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--data-valuation.py pipelines--openml--multiple.py 44 17322
-
 #PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17326
 #PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--fairness.py pipelines--openml--multiple.py V3 married 1461 17322
