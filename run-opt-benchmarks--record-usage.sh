ef1b61542da5915f9e5af2e195c25cca2ba909b3
sscdotopen <ssc.open@googlemail.com>
2022-05-29 07:08:59 +0200

    exp

--- run-opt-benchmarks--record-usage.sh
+++ run-opt-benchmarks--record-usage.sh
@@ -5,18 +5,18 @@
 #DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--dspipes--multiple.py adult num_pipe_0 logistic
 #DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--dspipes--multiple.py adult num_pipe_1 tree
 #DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--dspipes--multiple.py adult num_pipe_2 logistic
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--dspipes--multiple.py cardio-sampled num_pipe_0 logistic
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--dspipes--multiple.py cardio-sampled num_pipe_1 tree
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--dspipes--multiple.py cardio-sampled num_pipe_2 logistic
+#DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--dspipes--multiple.py cardio-sampled num_pipe_0 logistic
+#DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--dspipes--multiple.py cardio-sampled num_pipe_1 tree
+#DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--dspipes--multiple.py cardio-sampled num_pipe_2 logistic
 
 #DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17326
 #DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 44 17322
 #DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 44 8774
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 1461 17326
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 1461 17322
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 1461 8774
+#DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 1461 17326
+#DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 1461 17322
+#DONE PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--openml--multiple.py 1461 8774
 
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--mlinspect--amazon-reviews.py
+### PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--mlinspect--amazon-reviews.py
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--mlinspect--credit.py
 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python benchmarks--record-usage.py pipelines--mlinspect--product-images.py
 
