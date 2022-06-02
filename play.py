203a899e61ea446d10d515a6a1454126b4404ae8
sscdotopen <ssc.open@googlemail.com>
2022-06-02 17:40:42 +0200

    openml

--- play.py
+++ play.py
@@ -11,10 +11,10 @@ from freamon.compliance import FairnessMetrics, RecordUsage, DataValuation
 
 frm = Freamon()
 
-cmd_args = []#'1461', '18922']
-with frm.pipeline_from_py_file('pipelines--mlinspect--amazon-reviews.py', cmd_args=cmd_args) as pipeline:
-    pipeline.compute(FairnessMetrics('third_party', 'N'))
+cmd_args = ['44', '17322']
+with frm.pipeline_from_py_file('pipelines--openml--multiple.py', cmd_args=cmd_args) as pipeline:
+    #pipeline.compute(FairnessMetrics('third_party', 'N'))
     pipeline.compute(RecordUsage())
-    pipeline.compute(DataValuation())
+    #pipeline.compute(DataValuation())
 
     print(f"It worked")
