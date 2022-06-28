5a32d76652414feccbd269906575ed728b4d6e14
sscdotopen <ssc.open@googlemail.com>
2022-06-28 14:44:52 +0200

    first version of viewgen

--- freamon/extraction/with_mlinspect.py
+++ freamon/extraction/with_mlinspect.py
@@ -8,10 +8,7 @@ from mlinspect import PipelineInspector
 from mlinspect.inspections._lineage import RowLineage
 from mlinspect.inspections._inspection_input import OperatorType
 
-from freamon.templates.classification import ClassificationPipeline
-from freamon.extraction import feature_matrix_extractor, source_extractor
-from freamon.templates import Output
-from freamon.extraction._deserialise_provenance import to_polynomials
+from freamon.viewgen.duckdb import DuckDBViewGenerator
 
 
 def from_py_file(path_to_py_file, cmd_args=[]):
@@ -27,8 +24,7 @@ def from_py_file(path_to_py_file, cmd_args=[]):
 def _execute_pipeline(inspector: PipelineInspector):
     lineage_inspection = RowLineage(RowLineage.ALL_ROWS, [OperatorType.DATA_SOURCE, OperatorType.TRAIN_DATA,
                                                           OperatorType.TRAIN_LABELS, OperatorType.TEST_DATA,
-                                                          OperatorType.TEST_LABELS, OperatorType.SCORE,
-                                                          OperatorType.JOIN])
+                                                          OperatorType.TEST_LABELS, OperatorType.SCORE])
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -53,81 +49,5 @@ def _execute_pipeline(inspector: PipelineInspector):
         for node, node_results in result.dag_node_to_inspection_results.items()
     }
 
-    provenance_conversion_start = time.time()
-    dag_node_to_provenance = {
-        node: to_polynomials(node_result['mlinspect_lineage'])
-        for node, node_result in dag_node_to_intermediates.items() if node_result is not None
-    }
-    provenance_conversion_duration = time.time() - provenance_conversion_start
-    runtimes['provenance_conversion'] = provenance_conversion_duration * 1000
-
-    pipeline = _from_dag_and_lineage(result.dag, dag_node_to_intermediates, dag_node_to_provenance, runtimes)
-
-    return pipeline, runtimes
-
-
-
-def _from_dag_and_lineage(dag, dag_node_to_intermediates, dag_node_to_provenance, runtimes):
-    artifact_extraction_start = time.time()
-
-    logging.info(f'Identifying training sources')
-    train_source_identification_start = time.time()
-    train_sources, train_source_lineage = \
-        source_extractor.extract_train_sources(dag, dag_node_to_intermediates, dag_node_to_provenance)
-    train_source_identification_duration = time.time() - train_source_identification_start
-    logging.info(f'---RUNTIME: Artifact extraction > Train source extraction took {train_source_identification_duration * 1000} ms')
-    runtimes['train_source_extraction'] = train_source_identification_duration * 1000
-
-    logging.info(f'Identifying test sources')
-    test_source_identification_start = time.time()
-    test_sources, test_source_lineage = \
-        source_extractor.extract_test_sources(dag, dag_node_to_intermediates, dag_node_to_provenance)
-    test_source_identification_duration = time.time() - test_source_identification_start
-    logging.info(f'---RUNTIME: Artifact extraction > Test source extraction took {test_source_identification_duration * 1000} ms')
-    runtimes['test_source_extraction'] = test_source_identification_duration * 1000
-
-    feature_matrix_extraction_start = time.time()
-    X_train, lineage_X_train = \
-        feature_matrix_extractor.extract_train_feature_matrix(dag_node_to_intermediates, dag_node_to_provenance)
-    logging.info(f'Extracted feature matrix X_train with {len(X_train)} rows and {len(X_train[0])} columns')
-    X_test, lineage_X_test = \
-        feature_matrix_extractor.extract_test_feature_matrix(dag_node_to_intermediates, dag_node_to_provenance)
-    logging.info(f'Extracted feature matrix X_test with {len(X_test)} rows and {len(X_test[0])} columns')
-    feature_matrix_extraction_duration = time.time() - feature_matrix_extraction_start
-    logging.info(f'---RUNTIME: Artifact extraction > Feature matrix extraction took {feature_matrix_extraction_duration * 1000} ms')
-    runtimes['feature_matrix_extraction'] = feature_matrix_extraction_duration * 1000
-
-    label_prediction_extraction_start = time.time()
-    y_train, lineage_y_train = \
-        feature_matrix_extractor.extract_train_labels(dag_node_to_intermediates, dag_node_to_provenance)
-    y_test, lineage_y_test = \
-        feature_matrix_extractor.extract_test_labels(dag_node_to_intermediates, dag_node_to_provenance)
-    y_pred, lineage_y_pred = \
-        feature_matrix_extractor.extract_predicted_labels(dag_node_to_intermediates, dag_node_to_provenance)
-    label_prediction_extraction_duration = time.time() - label_prediction_extraction_start
-    logging.info(f'---RUNTIME: Artifact extraction > Label and prediction extraction took {label_prediction_extraction_duration * 1000} ms')
-    runtimes['label_and_prediction_extraction'] = label_prediction_extraction_duration * 1000
-
-    logging.info(f'Extracted y_train, y_test and y_pred')
-
-    outputs = {
-        Output.X_TRAIN: X_train,
-        Output.Y_TRAIN: y_train,
-        Output.X_TEST: X_test,
-        Output.Y_TEST: y_test,
-        Output.Y_PRED: y_pred
-    }    
-
-    output_lineage = {
-        Output.X_TRAIN: lineage_X_train,
-        Output.Y_TRAIN: lineage_y_train,
-        Output.X_TEST: lineage_X_test,
-        Output.Y_TEST: lineage_y_test,
-        Output.Y_PRED: lineage_y_pred
-    }    
-
-    artifact_extraction_duration = time.time() - artifact_extraction_start
-    logging.info(f'---RUNTIME: Artifact extraction took {artifact_extraction_duration * 1000} ms')
-
-    return ClassificationPipeline(train_sources, train_source_lineage, test_sources, test_source_lineage,
-                                  outputs, output_lineage, dag, dag_node_to_intermediates, dag_node_to_provenance)
+    return DuckDBViewGenerator(result.dag, dag_node_to_intermediates), runtimes
+    # return result.dag, dag_node_to_intermediates, runtimes
---
+++ freamon/viewgen/duckdb.py
@@ -0,0 +1,131 @@
+import logging
+import duckdb
+
+from mlinspect.inspections._inspection_input import OperatorType
+
+
+class DuckDBViewGenerator:
+
+    def __init__(self, dag, node_to_intermediates):
+        self.dag = dag
+        self.node_to_intermediates = node_to_intermediates
+
+        self.db = duckdb.connect(database=':memory:')
+        self.test_sources = {}
+
+        self._init_test_view()
+
+    def _init_test_view(self):
+
+        test_data_node = self._find_first_by_type(OperatorType.TEST_DATA)
+        self.test_sources = self._find_source_datasets(test_data_node.node_id)
+
+        # Register test_sources
+        for source_id, source in self.test_sources.items():
+            self.db.register(f'_freamon_source_{source_id}', source)
+            self.db.execute(f"""
+              CREATE OR REPLACE VIEW _freamon_source_{source_id}_with_prov_view AS 
+              SELECT 
+                * EXCLUDE mlinspect_lineage, 
+                CAST(string_to_array(trim(mlinspect_lineage, ')('), ',')[2] AS INT) AS prov_id_source_{source_id}
+              FROM _freamon_source_{source_id}
+            """)
+
+        # Register X_test with y_true and y_pred
+        x_test = self.node_to_intermediates[self._find_first_by_type(OperatorType.TEST_DATA)]
+        y_test = self.node_to_intermediates[self._find_first_by_type(OperatorType.TEST_LABELS)]
+        y_pred = self.node_to_intermediates[self._find_first_by_type(OperatorType.SCORE)]
+
+        # TODO can we do this without copies?
+        x_test.rename(columns={'array': 'features'}, inplace=True)
+        x_test['y_true'] = y_test['array']
+        x_test['y_pred'] = y_pred['array']
+
+        self.db.register(f'_freamon_x_test', x_test)
+
+        # Create a view with foreign keys over the test data
+        provenance_based_fk_columns = ''
+
+        for source_index in range(len(self.test_sources)):
+            provenance_based_fk_columns += \
+                f"CAST(regexp_replace(polynomial[{source_index + 1}], '\(\\d+,|\)', '', 'g') AS INT) " + \
+                f"AS prov_id_source_{source_index},\n"
+
+        # TODO this might have some issues if the same table is joined twice, because we lose the order in list_distinct
+        self.db.execute(f"""
+          CREATE OR REPLACE VIEW _freamon_test_view AS 
+          SELECT 
+            features, y_true, y_pred,
+            {provenance_based_fk_columns}    
+          FROM (
+            SELECT 
+              features, y_true, y_pred,
+              list_sort(list_distinct(string_to_array(mlinspect_lineage, ';'))) AS polynomial
+            FROM _freamon_x_test
+          )
+        """)
+
+    def test_view(self, sliceable_by, with_features, with_y_true, with_y_pred):
+
+        columns_to_select = []
+        sources_to_join = set()
+
+        for slice_column in sliceable_by:
+            for source_index, source in self.test_sources.items():
+                if slice_column in list(source.columns):
+                    columns_to_select.append(f"fs{source_index}.{slice_column}")
+                    sources_to_join.add(source_index)
+                    break
+
+        if with_features:
+            columns_to_select.append('ftv.features')
+            
+        if with_y_true:
+            columns_to_select.append('ftv.y_true')
+
+        if with_y_pred:
+            columns_to_select.append('ftv.y_pred')
+
+        source_joins = ''
+        for source_index in sources_to_join:
+            source_joins += \
+                f"JOIN _freamon_source_{source_index}_with_prov_view fs{source_index} " + \
+                f" ON fs{source_index}.prov_id_source_{source_index} = ftv.prov_id_source_{source_index}\n"
+
+        query = f"""
+SELECT {', '.join(columns_to_select)}
+FROM _freamon_test_view ftv
+{source_joins}   
+        """
+
+        logging.info(query)
+
+        return self.db.execute(query).df()
+
+
+    def _find_source_datasets(self, start_node_id):
+        nodes_to_search = []
+        nodes_processed = set()
+
+        source_datasets = {}
+
+        nodes_to_search.append(start_node_id)
+
+        while len(nodes_to_search) > 0:
+            current_node_id = nodes_to_search.pop()
+            for source, target in self.dag.edges:
+                if target.node_id == current_node_id:
+                    if source.node_id not in nodes_processed and source.node_id not in nodes_to_search:
+                        nodes_to_search.append(source.node_id)
+                        if source.operator_info.operator == OperatorType.DATA_SOURCE:
+                            data = self.node_to_intermediates[source]
+                            source_datasets[source.node_id] = data
+            nodes_processed.add(current_node_id)
+
+        return source_datasets
+
+    def _find_first_by_type(self, operator_type):
+        for node in self.dag.nodes:
+            if node.operator_info.operator == operator_type:
+                return node
+        raise ValueError(f'Unable to find DAG node of type {operator_type}')
